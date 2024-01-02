1a59751e493d60ea7b398a0352cfab64138763fe
Leandro Sander Manzieri <lmanzieri@gmail.com>
2024-01-02 17:57:41 -0300

    add cloud nat router config

--- network.tf
+++ network.tf
@@ -75,3 +75,26 @@ module "vpc" {
   depends_on = [google_project_service.compute]
 
 }
+
+resource "google_compute_router" "router" {
+  name    = "${var.name}-${var.env}-router"
+  region  = var.region
+  network = module.vpc.network_name
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "router_nat" {
+  name                               = "${var.name}-${var.env}-router-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
